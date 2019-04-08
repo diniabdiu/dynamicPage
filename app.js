28c12fbfcffcd061bb66596655eb84c144fffb86
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-08 19:56:23 +0200

    Styling campgrounds

--- app.js
+++ app.js
@@ -8,17 +8,22 @@ app.set('view engine', 'ejs');
 app.get('/', function(req, res) {
 res.render('landing');
 });
-app.get('/campground', function(req, res) {
-    var campgrounds = [
-        {name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'},
-        {name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4044/4175370953_5488caf554.jpg'},
-        {name: 'Salmon Creek', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg'}
+var campgrounds = [
+    {name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'},
+    {name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4044/4175370953_5488caf554.jpg'},
+    {name: 'Salmon Creek', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg'}
 
-    ]
+];
+app.get('/campground', function(req, res) {
     res.render('campgrounds', {campgrounds: campgrounds});
 });
 app.post('/campgrounds', function(req, res) {
     res.send('YOU HIT THE POST ROUTE!');
+    var name = req.body.name
+    var image = req.body.image
+    var newCampground = {name: name, image: image}
+    campgrounds.push(newCampground);
+    res.redirect('/campgrounds');
 });
 app.get('/campgrounds/new', function(req, res) {
     res.render('new.ejs');
