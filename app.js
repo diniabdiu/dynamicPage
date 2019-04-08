2911c6e25f1ce842bf090e3302e9fe67316adc54
Hajredin Abidu <diniabdiu@gmail.com>
2019-04-08 18:16:27 +0200

    Created layout

---
+++ app.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+res.render('landing');
+});
+app.get('/campground', function(req, res) {
+    var campgrounds = [
+        {name: 'Salmon Creek', image: 'https://farm2.staticflickr.com/1424/1430198323_c26451b047.jpg'},
+        {name: 'Salmon Creek', image: 'https://farm5.staticflickr.com/4044/4175370953_5488caf554.jpg'},
+        {name: 'Salmon Creek', image: 'https://farm6.staticflickr.com/5108/5789045796_27c9217bf2.jpg'}
+
+    ]
+    res.render('campgrounds', {campgrounds: campgrounds});
+});
+app.post('/campgrounds', function(req, res) {
+    res.send('YOU HIT THE POST ROUTE!');
+});
+app.get('/campgrounds/new', function(req, res) {
+    res.render('new.ejs');
+});
+
+
+const PORT = parseInt(process.env.PORT);
+
+app.listen(parseInt(PORT), process.env.ip, function(){
+
+    console.log('The YelpCamp server has started');
+    console.log(`Process is litesning to http://localhost:${PORT}`);
+});
+
+
